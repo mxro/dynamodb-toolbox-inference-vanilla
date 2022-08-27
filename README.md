35bd9381fae8a74d2061ea5da6bb4b121a74b105
Max Rohde <maxrohde.public@gmail.com>
2022-08-28 05:50:20 +1000

    Uploading first version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # dynamodb-toolbox-inference-vanilla
+
+Testing a vanilla project for DynamoDB Toolbox [#261](https://github.com/jeremydaly/dynamodb-toolbox/issues/261#issuecomment-1229251645)
+
+## Setup
+
+- Clone project
+- Run `npm`
+- Check type inference in `src/app.ts` and `src/test.spec.ts`
---
+++ src/test.spec.ts
@@ -0,0 +1,36 @@
+import { DynamoDB } from "aws-sdk";
+import { Entity, Table } from "dynamodb-toolbox";
+
+it('Should type infer', async () => {
+    const table = new Table({
+      name: 'table',
+      partitionKey: 'pk',
+      sortKey: 'sk',
+      DocumentClient: new DynamoDB.DocumentClient({}),
+    });
+
+    const e = new Entity({
+      name: 'User',
+      attributes: {
+        pk: { partitionKey: true },
+        sk: { hidden: true, sortKey: true },
+        name: { type: 'string', required: true },
+        emailVerified: { type: 'boolean', required: true },
+      },
+      table,
+    } as const);
+
+    await e.put({
+      pk: 'joe@email.com',
+      sk: 'admin',
+      name: 'Joe',
+      emailVerified: true,
+    });
+
+    const { Item: user } = await e.get(
+      { pk: 'joe@email.com', sk: 'admin' },
+      { attributes: ['name', 'pk'] }
+    );
+
+    console.log(user?.name);
+});
